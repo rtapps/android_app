3ee931d10b4530eea16dc11bfac8d02ff151e25b
Royee Tichauer <Royee.Tichauer@vonage.com>
2016-09-09 14:01:46 +0300

    אשע

---
+++ AndroidApp/app/src/main/java/rtapps/app/inbox/Tag.java
@@ -0,0 +1,35 @@
+package rtapps.app.inbox;
+
+import com.rtapps.kingofthejungle.R;
+
+public class Tag{
+
+    private  int tagId;
+    private  String tagName;
+
+    public Tag(int tagId , String tagName){
+        this.tagId = tagId;
+        this.tagName = tagName;
+    }
+
+    public int getTagId(){
+            return  tagId;
+        }
+
+    public String tagName(){
+            return tagName;
+        }
+
+    public static final Tag[] tagCollection = {
+            new Tag(R.drawable.tag_10,"10_percent"),
+            new Tag(R.drawable.tag_20,"20_percent"),
+            new Tag(R.drawable.tag_30,"30_percent"),
+            new Tag(R.drawable.tag_40,"40_percent"),
+            new Tag(R.drawable.tag_50,"50_percent"),
+            new Tag(R.drawable.tag_60,"60_percent"),
+            new Tag(R.drawable.tag_70,"70_percent"),
+            new Tag(R.drawable.tag_80,"80_percent"),
+            new Tag(R.drawable.tag_90,"90_percent"),
+    };
+
+}
