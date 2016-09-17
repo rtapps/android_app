9b766920006cc36ed33501a2bcbf43c7da380ada
Royee Tichauer <Royee.Tichauer@vonage.com>
2016-09-17 17:59:34 +0300

    Add also for customer

--- AndroidApp/app/src/customer/java/rtapps/app/ui/InboxFragment.java
+++ AndroidApp/app/src/customer/java/rtapps/app/ui/InboxFragment.java
@@ -1,9 +1,34 @@
 package rtapps.app.ui;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.squareup.otto.Subscribe;
+
+import rtapps.app.infrastructure.BusHolder;
+import rtapps.app.services.MessagesSynchronizer;
 
 /**
  * Created by rtichauer on 8/13/16.
  */
 public class InboxFragment extends InboxFragmentBase {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        BusHolder.get().getBus().register(this);
+//        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        BusHolder.get().getBus().unregister(this);
+    }
+    @Subscribe
+    public void onMessagesSync(MessagesSynchronizer.MessagesSyncEvent event) {
+        Log.d("InboxFragmentBase", "Received MessagesSyncEvent. Reloading adapter");
+        new ReadMessagesFromDb().execute();
+    }
+
 }
