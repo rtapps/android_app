12d50716935ec276bb214308241ac431b3f40792
Tzachi Azo <tazo@vonage.com>
2016-09-22 23:40:51 +0300

    Added Tags

--- AndroidApp/app/src/main/java/rtapps/app/inbox/Tag.java
+++ AndroidApp/app/src/main/java/rtapps/app/inbox/Tag.java
@@ -21,6 +21,9 @@ public class Tag{
         }
 
     public static final Tag[] tagCollection = {
+            new Tag(R.drawable.tag_pessy_eng,"tag_pessy_eng"),  //todo - per business
+            new Tag(R.drawable.tag_pessy_heb,"tag_pessy_heb"),  //todo - per business
+
             new Tag(R.drawable.tag_10,"10_percent"),
             new Tag(R.drawable.tag_20,"20_percent"),
             new Tag(R.drawable.tag_30,"30_percent"),
@@ -30,6 +33,16 @@ public class Tag{
             new Tag(R.drawable.tag_70,"70_percent"),
             new Tag(R.drawable.tag_80,"80_percent"),
             new Tag(R.drawable.tag_90,"90_percent"),
+            new Tag(R.drawable.tag_1_1,"tag_1_1"),
+            new Tag(R.drawable.tag_2_1,"tag_2_1"),
+            new Tag(R.drawable.tag_3_1,"tag_3_1"),
+            new Tag(R.drawable.tag_4_1,"tag_4_1"),
+            new Tag(R.drawable.tag_5_1,"tag_5_1"),
+
+            new Tag(R.drawable.tag_discount_heb,"tag_discount_heb"),
+            new Tag(R.drawable.tag_new_eng,"tag_new_eng"),
+            new Tag(R.drawable.tag_new_heb,"tag_new_heb"),
+
     };
 
 }
