ced43601b8041a65d084b7b392cf784e4368cadc
Tzachi Azo <tazo@vonage.com>
2016-09-24 15:08:42 +0300

    store info string

--- AndroidApp/app/src/main/java/rtapps/app/ui/StoreInfoFragment.java
+++ AndroidApp/app/src/main/java/rtapps/app/ui/StoreInfoFragment.java
@@ -101,53 +101,6 @@ public class StoreInfoFragment extends Fragment {
         return v;
     }
 
-    private void addStores(View v, LayoutInflater inflater) {
-
-        LinearLayout storeInfoContainer = (LinearLayout) v.findViewById(R.id.stores_info);
-
-        //String[] storeName = {R.string.info_store1_name , ""};
-
-        //for(int i = 0 ; i < 2 ; i++){
-        LinearLayout ll = (LinearLayout) inflater.inflate(R.layout.store_info, storeInfoContainer, false);
-
-        ((TextView) ll.findViewById(R.id.sore_info_name)).setText(R.string.info_store1_name);
-        ((TextView) ll.findViewById(R.id.sore_info_address)).setText(R.string.info_store1_address);
-
-        storeInfoContainer.addView(ll);
-
-        View ll3 = inflater.inflate(R.layout.seprator_line, storeInfoContainer, false);
-        storeInfoContainer.addView(ll3);
-
-        LinearLayout ll1 = (LinearLayout) inflater.inflate(R.layout.store_info, storeInfoContainer, false);
-
-        ((TextView) ll1.findViewById(R.id.sore_info_name)).setText(R.string.info_store2_name);
-        ((TextView) ll1.findViewById(R.id.sore_info_address)).setText(R.string.info_store2_address);
-
-        storeInfoContainer.addView(ll1);
-
-        View ll5 = inflater.inflate(R.layout.seprator_line, storeInfoContainer, false);
-        storeInfoContainer.addView(ll5);
-
-        LinearLayout ll9 = (LinearLayout) inflater.inflate(R.layout.store_info, storeInfoContainer, false);
-
-        ((TextView) ll9.findViewById(R.id.sore_info_name)).setText(R.string.info_store2_name);
-        ((TextView) ll9.findViewById(R.id.sore_info_address)).setText(R.string.info_store2_address);
-
-        storeInfoContainer.addView(ll9);
-
-        View ll83 = inflater.inflate(R.layout.seprator_line, storeInfoContainer, false);
-        storeInfoContainer.addView(ll83);
-
-        LinearLayout ll14 = (LinearLayout) inflater.inflate(R.layout.store_info, storeInfoContainer, false);
-
-        ((TextView) ll14.findViewById(R.id.sore_info_name)).setText(R.string.info_store2_name);
-        ((TextView) ll14.findViewById(R.id.sore_info_address)).setText(R.string.info_store2_address);
-
-        storeInfoContainer.addView(ll14);
-        //}
-
-
-    }
 
 
 }
