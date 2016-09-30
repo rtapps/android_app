d58a6aa1fb317fd9aa6ff068e4ccb1e8e585ba66
Royee Tichauer <Royee.Tichauer@vonage.com>
2016-09-30 22:56:04 +0300

    Updated notification type

--- AndroidApp/app/src/main/java/rtapps/app/gcm/MyGcmListenerService.java
+++ AndroidApp/app/src/main/java/rtapps/app/gcm/MyGcmListenerService.java
@@ -21,6 +21,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -28,10 +30,23 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GcmListenerService;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.raizlabs.android.dbflow.sql.language.Condition;
+import com.raizlabs.android.dbflow.sql.language.Select;
 import com.rtapps.buisnessapp.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.util.List;
 
 import rtapps.app.config.ApplicationConfigs;
 import rtapps.app.config.Configurations;
+import rtapps.app.databases.MessagesTable;
+import rtapps.app.databases.MessagesTable_Table;
+import rtapps.app.services.MessagesSynchronizer;
 import rtapps.app.services.SyncDataService;
 import rtapps.app.ui.MainActivity;
 
@@ -70,6 +85,52 @@ public class MyGcmListenerService extends GcmListenerService {
      * @param message GCM message received.
      */
     private void sendNotification(String message) {
+
+        NotificationCompat.Builder notificationBuilder = getNotificationBuilder();
+        try {
+            JSONObject jsonObject = new JSONObject(message);
+
+            String id = jsonObject.get("id").toString();
+            MessagesSynchronizer messagesSynchronizer = new MessagesSynchronizer(this);
+            messagesSynchronizer.syncAllMessages();
+
+            List<MessagesTable> messages = new Select().from(MessagesTable.class).where(MessagesTable_Table.id.is(id)).queryList();
+
+            if (messages.size() > 1){
+                throw new RuntimeException("More than one message found");
+            }
+
+
+            if (messages.size() == 1){
+
+                notificationBuilder.setStyle(getBigPictureStyle(messages.get(0)));
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+    private NotificationCompat.Style getBigPictureStyle(MessagesTable messagesTable) {
+        NotificationCompat.BigPictureStyle bigPictureStyle = new NotificationCompat.BigPictureStyle();
+        File directory = getDir("messages", Context.MODE_PRIVATE);
+        File file = new File(directory, messagesTable.getId() +"/" +messagesTable.getFullImageName());
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        Bitmap bitmap = BitmapFactory.decodeFile(file.getPath(), options);
+        bigPictureStyle.bigPicture(bitmap);
+        bigPictureStyle.setBigContentTitle(messagesTable.getHeader());
+        bigPictureStyle.setSummaryText(messagesTable.getBody());
+        return bigPictureStyle;
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder(){
+
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         intent.putExtra("Notification", true);
@@ -77,20 +138,14 @@ public class MyGcmListenerService extends GcmListenerService {
                 PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+        return new NotificationCompat.Builder(this)
                 //TODO set small icon
                 .setSmallIcon(R.drawable.ic_stat_image_tag_faces)
                 .setContentTitle(ApplicationConfigs.getBusinessName())
-                .setContentText(message)
                 .setAutoCancel(true)
                 .setDefaults(Notification.DEFAULT_ALL) // must requires VIBRATE permission
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 }
